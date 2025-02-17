f53fe0a618187a1150e9c30dab7030aac65d9ef8
bernard oddiah <home.oddiah@gmail.com>
2025-02-17 11:32:37 +0100

    Added workflow other config files

---
+++ IAC/SG.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "microk8s_sg" {
+  name        = "microk8s-security-group"
+  description = "Allow SSH and Kubernetes access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Change for security
+  }
+
+  ingress {
+    from_port   = 16443
+    to_port     = 16443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # ingress {
+  #   from_port   = 443
+  #   to_port     = 443
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ IAC/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "microk8s" {
+  ami           = "ami-04b4f1a9cf54c11d0" 
+  instance_type = "t2.micro"
+  key_name      = "microk8s_key"
+
+  associate_public_ip_address = true
+
+  security_groups = [aws_security_group.microk8s_sg.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install -y snapd
+              sudo snap install microk8s --classic
+              sudo usermod -aG microk8s ubuntu
+              sudo chown -R ubuntu ~/.kube
+              microk8s status --wait-ready
+              microk8s enable dns dashboard storage
+              EOF
+
+  tags = {
+    Name = "MicroK8s-Instance"
+  }
+}
