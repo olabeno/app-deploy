a9a5f977142665fc54635f8d0a4cec180ddffcf8
Bernard Olatunde Oddiah <bernard.oddiah@gmail.com>
2025-02-18 08:16:37 +0100

    Update backend.tf

--- IAC/backend.tf
+++ IAC/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "iac-state-bucket001"
-    key            = ""terraform/state.tfstate""
+    key            = "terraform/state.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
