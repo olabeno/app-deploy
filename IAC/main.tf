911dbb178a11333173f137d7f79c3a51aba3af30
Bernard Olatunde Oddiah <bernard.oddiah@gmail.com>
2025-02-18 18:07:14 +0100

    Update main.tf

--- IAC/main.tf
+++ IAC/main.tf
@@ -16,7 +16,8 @@ resource "aws_instance" "microk8s" {
               sudo chown -R ubuntu ~/.kube
               microk8s status --wait-ready
               microk8s enable dns dashboard storage
-              microk8s config > ~/.kube/config
+              microk8s kubectl config view --raw > /home/ubuntu/.kube/config
+              chown ubuntu:ubuntu /home/ubuntu/.kube/config
               EOF
 
   tags = {
