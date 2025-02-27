9d70d5803d70def19a21d0c94ae588495c5d4216
Bernard Olatunde Oddiah <bernard.oddiah@gmail.com>
2025-02-27 15:54:07 +0100

    Update SG.tf

--- IAC/SG.tf
+++ IAC/SG.tf
@@ -23,12 +23,12 @@ resource "aws_security_group" "microk8s_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # ingress {
-  #   from_port   = 443
-  #   to_port     = 443
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
+  ingress {
+     from_port   = 443
+     to_port     = 443
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
 
   egress {
     from_port   = 0
